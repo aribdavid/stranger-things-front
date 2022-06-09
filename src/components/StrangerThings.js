b7f65e13648b112e7a26fb94583e73ca8fe174ec
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-09 17:40:51 -0300

    feat: fix 3

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,7 +7,7 @@ const {
   REACT_APP_UPSIDEDOWN_URL,
   REACT_APP_HAWKINS_TIMEOUT,
   REACT_APP_UPSIDEDOWN_TIMEOUT,
-  REACT_APP_DEV,
+  // REACT_APP_DEV,
 } = process.env;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
@@ -121,7 +121,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        { REACT_APP_DEV === 'true' && <h1>Em desenvolvimento</h1> }
+        {/* { REACT_APP_DEV === 'true' && <h1>Em desenvolvimento</h1> } */}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
